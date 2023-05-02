1b4e98b36dac6ca46f17d4b9ff9a0c5e15ea9fd9
Josep <josebu74@outlook.com>
2023-05-02 15:36:55 +0200

    More renaming

--- EndScreen.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-
-func _on_button_button_up():
-	get_tree().quit()
--- MainMenu.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-func _on_butt_new_g_button_up():
-	get_tree().change_scene_to_file("res://LootPhase/LootPhase.tscn")
